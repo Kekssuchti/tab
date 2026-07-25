fix preprocessing issues: - convert differnt UREA units - reorder filtering - fix age limit - improve code structure for it too
run baseline mortality with "new" data

src/exploration/data_filtering.py
@@ -14,8 +14,9 @@ def _():
 
     from src.config import config
     from src.utils.dataset_utils import standard_preprocessing
+    import dtale
 
-    return config, pd, standard_preprocessing
+    return config, dtale, pd, standard_preprocessing
 
 
 @app.cell
@@ -29,34 +30,33 @@ def _(config, pd):
     df_mimic_read = pd.read_csv(
         config.dir_data / "extracted" / "mimic4_readmission.csv"
     )
-    return df_tudd, df_tudd_read
 
 
-@app.function
-def filter_many_missing(df, threshold_row=0.5, threshold_col=0.5):
-    print("shape before missing filter", df.shape)
-
-    df_features = df.drop(columns=["mortality", "LOS"], inplace=False)
-    df = df.loc[:, ~df.columns.str.contains("^Unnamed")]
-
-    row_null = df.isnull().sum(axis=1)
-    df = df[row_null < (threshold_row * len(df.columns))]
-
-    df = df.loc[:, df.isnull().mean() < 0.5]
-
-    print("shape after missing filter", df.shape)
-    return df
+    df_tudd_fil_file = pd.read_csv(config.dir_data / "filtered" / "tudd_mean_100_full.csv")
+    return df_mimic, df_mimic_read, df_tudd, df_tudd_read
 
 
 @app.cell
-def _(df_tudd):
-    df_tudd.isnull().mean() < 0.5
+def _(df_tudd, dtale):
+    dtale.show(df_tudd)
+    return
+
+
+@app.cell
+def _(df_mimic_read, dtale):
+    dtale.show(df_mimic_read)
+    return
+
+
+@app.cell
+def _(df_tudd_read, dtale):
+    dtale.show(df_tudd_read)
     return
 
 
 @app.cell
 def _(config, df_tudd, pd, standard_preprocessing):
-    df_tudd_filter = standard_preprocessing(df_tudd, readmission=False)
+    df_tudd_filter = standard_preprocessing(df_tudd, "tudd", readmission=False)
 
     df_tudd_filt = pd.read_csv(config.dir_data / "filtered" / "tudd_mean_100_full.csv")
 
@@ -70,8 +70,38 @@ def _(config, df_tudd, pd, standard_preprocessing):
 
 
 @app.cell
+def _(df_tudd):
+    df_tudd["Urea+100%mean"].describe()
+    return
+
+
+@app.cell
+def _(df_tudd_filt):
+    df_tudd_filt["Urea+100%mean"].describe()
+    return
+
+
+@app.cell
+def _(df_tudd_filter):
+    df_tudd_filter["Urea+100%mean"].describe()
+    return
+
+
+@app.cell
+def _(df_mimic):
+    df_mimic["Urea+100%mean"].describe()
+    return
+
+
+@app.cell
+def _(df_tudd_filt):
+    df_tudd_filt.columns
+    return
+
+
+@app.cell
 def _(config, df_tudd_read, pd, standard_preprocessing):
-    df_tudd_read_filter = standard_preprocessing(df_tudd_read, readmission=False)
+    df_tudd_read_filter = standard_preprocessing(df_tudd_read, "tudd", readmission=True)
 
     df_tudd_read_filt = pd.read_csv(
         config.dir_data / "filtered" / "tudd_readmission.csv"
@@ -83,6 +113,12 @@ def _(config, df_tudd_read, pd, standard_preprocessing):
 
     print(df_tudd_read_filt.shape)
     print(df_tudd_read_filter.shape)
+    return (df_tudd_read_filter,)
+
+
+@app.cell
+def _(df_tudd_read_filter, dtale):
+    dtale.show(df_tudd_read_filter)
     return
 
 
@@ -99,7 +135,6 @@ def _(df_tudd_filt, df_tudd_filter):
 def _(df_tudd_filt):
     df_tudd_filt["Sex"].unique()
     df_tudd_filt["Sex"] = (df_tudd_filt["Sex"] == "F").astype(int)
-
     return
 
 
@@ -116,9 +151,7 @@ def _(df_tudd_filter):
 
 
 @app.cell
-def _(df_tudd_filt):
-    df_tudd_filt["Age"] = min(df_tudd_filt["Age"], 90)
-    df_tudd_filt["Age"]
+def _():
     return
 
 

src/utils/dataset_utils.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 
 from src.config import config
-from src.schemas.dataset_schemas import DatasetPartSummary, XYDataset
+from src.schemas.dataset_schemas import DatasetOrigin, DatasetPartSummary, XYDataset
 from src.utils.logger import logger
 
 
@@ -45,34 +45,64 @@ def remove_impossible_values(df, json_file_path):
     return df, removed_counts
 
 
+def remove_unused_columns(df, cols):
+    # this assumes all cols we dont filter for are not worth keeping
+    cols_before = df.columns.tolist()
+
+    for column in cols_before:
+        if column not in cols:
+            df = df.drop(columns=[column], errors="ignore")
+
+    logger.info(f"dropped columns: {set(cols_before) - set(df.columns.tolist())}")
+
+    return df
+
+
+def _get_cols_from_json(json_file_path, is_readmission):
+    with open(json_file_path, "r") as file:
+        cols_json = json.load(file)
+
+    if is_readmission:
+        cols = cols_json["readmission"]
+    else:
+        cols = cols_json["normal"]
+    return cols
+
+
+def _get_feature_cols(cols, is_readmission):
+    cols_to_drop = ["mortality", "hours_to_readmit"]
+
+    if not is_readmission:
+        cols_to_drop.extend(["LOS"])
+
+    feature_cols = [col for col in cols if col not in cols_to_drop]
+    return feature_cols
+
+
+def _convert_units(df, dataset_origin):
+    # most of this is already done from the data I recieve
+    # only urea is not correctly converted
+    # this is also only applied to tudd datasets to convert TOTAL UREA to BUN
+    if dataset_origin == "tudd":
+        df["Urea+100%mean"] = df["Urea+100%mean"] / 2.1428
+    return df
+
+
 def _filter_many_missing(
     df: pd.DataFrame,
     readmission: bool,
+    feature_cols: list[str],
     threshold_row=0.5,
 ):
     logger.debug(f"shape before missing filter {df.shape}")
-    # -3 is offset for cols we dont use
     if readmission:
-        # mortality, LOS, Bmi+100%mean, hours_to_readmit
-        feature_offset = 4
-
-        # subject_id, hadm_id, stay_id dropped
-        df = df.drop(columns=["subject_id", "hadm_id", "stay_id"])
-
         # drop dead patients -> cannot readmit
-        df = df.loc[df["mortality"] != 1]
-    else:
-        # mortality, LOS, Bmi+100%mean
-        feature_offset = 3
+        df = df.loc[df["mortality"] == 0].copy()
+        df = df.drop(columns=["mortality"])
 
-    row_null: pd.Series = df.isnull().sum(axis=1)
-    df = df.loc[row_null < int(threshold_row * (len(df.columns) - feature_offset))]
+    missing_frac = df[feature_cols].isnull().mean(axis=1)
 
-    # REMOVED FOR NOW:
-    # Problem is that both datasets have different cols where they miss a lot of values
-
-    # drop all cols with too many missing but keep readmission for obvious reasons
-    # df = df.loc[:, (df.isnull().mean() < 0.5) | df.columns.isin(["hours_to_readmit"])]
+    df = df.loc[missing_frac <= threshold_row].copy()
 
     logger.debug(f"shape after missing filter {df.shape}")
     return df
@@ -88,24 +118,35 @@ def _filter_reasonable_los(df, min_h, max_h):
 
 def _filter_childs(df, min_age):
     logger.debug(f"shape before min age filter {df.shape}")
-    df = df[df["Age"] > min_age]
+    df = df[df["Age"] >= min_age]
     logger.debug(f"shape before min age filter {df.shape}")
     return df
 
 
+def _clip_max_age(df, max_age):
+    logger.debug(f"shape before max age filter {df.shape}")
+    df["Age"] = df["Age"].clip(upper=max_age)
+    logger.debug(f"shape after max age filter {df.shape}")
+    return df
+
+
 def _clean_dtypes(df: pd.DataFrame):
-    df["Sex"] = (df["Sex"] == "F").astype(int)
+    mapping = {"M": 0, "F": 1}
+    df["Sex"] = df["Sex"].map(mapping, na_action="ignore")
     return df
 
 
 def standard_preprocessing(
     df,
+    df_origin: DatasetOrigin,
     readmission: bool,
     threshold_row: float = 0.5,
     data_limit_config_path: Path = config.dir_configs / "data_limits.json",
+    data_cols_config_path: Path = config.dir_configs / "data_cols.json",
     min_los_filter=24,
-    max_los_filter=24 * 14000,
+    max_los_filter=24 * 100,
     min_age_filter=18,
+    max_age_filter=91,
 ):
     """
     Args:
@@ -120,16 +161,35 @@ def standard_preprocessing(
     It filters for minimum LOS 24h
     It removes rows (samples) with more missing values than threshold_row (default 50%)
     It removes cols (features) with more missing values than threshold_col (default 50%)
+    It clips max age to max_age_filter (default 91) - confirms with MIMIC Age tracking
     """
-
+    logger.debug(f"starting df len: {df.shape}")
     df.replace([np.inf, -np.inf], np.nan, inplace=True)
 
+    df = _convert_units(df, df_origin)
     df, rm_count = remove_impossible_values(df, data_limit_config_path)
-    logger.info(f"removed {rm_count} unreasonable values:")
+    logger.debug(f"removed {rm_count} unreasonable values:")
+    logger.debug(f"df len after remove_impossible_values: {len(df)}")
+
+    all_cols = _get_cols_from_json(data_cols_config_path, readmission)
+    feature_cols = _get_feature_cols(all_cols, readmission)
+
+    df = remove_unused_columns(df, all_cols)
+    logger.debug(f"df len after remove_unused_columns: {df.shape}")
+
     df = _filter_reasonable_los(df, min_los_filter, max_los_filter)
+    logger.debug(f"df len after _filter_reasonable_los: {len(df)}")
+
     df = _filter_childs(df, min_age=min_age_filter)
-    df = _filter_many_missing(df, readmission, threshold_row)
+    logger.debug(f"df len after _filter_childs: {len(df)}")
+
+    df = _clip_max_age(df, max_age=max_age_filter)
+
+    df = _filter_many_missing(df, readmission, feature_cols, threshold_row)
+    logger.debug(f"df len after _filter_many_missing: {len(df)}")
+
     df = _clean_dtypes(df)
+    logger.debug(f"df len after _clean_dtypes: {len(df)}")
 
     # still wip, we have minimally less rows than expected
     return df